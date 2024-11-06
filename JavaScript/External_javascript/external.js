3f03d1ee9bf27d66cd5d1abe30301b3e69a626ca
sripradesh <sripradeshm@gmail.com>
2024-11-06 11:23:45 +0530

    add javascript introduction and variable keywords

---
+++ JavaScript/External_javascript/external.js
@@ -0,0 +1,3 @@
+console.log("Hii sri!")
+console.log("This is your external java script file")
+console.log("Go on Coding!")
---
+++ JavaScript/variable_keywords.js
@@ -0,0 +1,16 @@
+var a; // declare
+a = 10; // initialisation
+a = 20; // re initialisation
+var a = 30; // re declare
+console.log(a); // print function
+
+let b; // declare
+b = 30; // initialisation
+b = "hi"; // re initialisation
+// let b = true; // !re declare
+console.log(b); // print function
+
+const c = 100;
+// const c = "b"; // ! re  declare
+c = "hi"; // ! re initialisation
+console.log(c); // print function
