c4af2fb1ef007c035a2d04c0dc0b20c75fe6e51a
sripradesh <sripradeshm@gmail.com>
2024-11-08 11:38:26 +0530

    add data types of java script

---
+++ JavaScript/DataTypes.js
@@ -0,0 +1,29 @@
+let a = 10;
+console.log(typeof (a));
+let b = .23;
+console.log(typeof b);
+let str = "12";
+console.log(a+str);
+console.log(a-str);
+let str1 = "";
+console.log(typeof str1);
+let c = true;
+console.log(typeof c);
+console.log(true+false);
+console.log(true+true);
+
+let d = null;
+console.log(d);
+console.log(typeof d);
+
+let e
+console.log(typeof e);
+
+console.log(typeof str);
+
+
+
+
+
+
+
