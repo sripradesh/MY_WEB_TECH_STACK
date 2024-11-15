ac982748a9c6207d49fedc0b63f40d65e1874df6
sripradesh <sripradeshm@gmail.com>
2024-11-15 11:20:48 +0530

    add constructor by class and functions

---
+++ JavaScript/Constructor/constructor_class.js
@@ -0,0 +1,11 @@
+class car{
+    constructor(id,name,price,engine){
+        this.id = id
+        this.name = name
+        this.price = price
+        this.engine = engine
+}
+}
+
+let car1 = new car(306,"FORTUNER","54,000","750cc")
+console.log(car1);
---
+++ JavaScript/Constructor/constructor_function.js
@@ -0,0 +1,9 @@
+function student(id,name,marks,subject){
+this.id = id
+this.name = name  
+this.marks = marks
+this.subject = subject
+}
+
+let stu1 = new student(306,"SRIPRADESH",99,"WEB")
+console.log(stu1);
